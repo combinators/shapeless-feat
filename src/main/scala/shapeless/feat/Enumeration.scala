5914fce60b9835f1f7767da3f1fb24dd636c6c2e
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2015-10-22 18:23:56 +0200

    BigInt instead of Long and some standard instances

--- src/main/scala/shapeless/feat/Enumeration.scala
+++ src/main/scala/shapeless/feat/Enumeration.scala
@@ -3,46 +3,49 @@ package shapeless.feat
 import scala.annotation.tailrec
 
 sealed abstract class Finite[+A] { self =>
-  val cardinality: Long
-  def get(idx: Long): A
+  val cardinality: BigInt
+  def get(idx: BigInt): A
   final def map[B](f: A => B) =
     new Finite[B] {
       val cardinality = self.cardinality
-      def get(idx: Long) = f (self.get(idx))
+      def get(idx: BigInt) = f (self.get(idx))
     }
   final def :+:[B >: A](left: Finite[B]): Finite[B] =
     new Finite[B] {
       val cardinality = left.cardinality + self.cardinality 
-      def get(idx: Long) =
+      def get(idx: BigInt) =
         if (idx < left.cardinality) left.get(idx) else self.get(idx - left.cardinality)
     }
   
   final def :*:[B](left: Finite[B]): Finite[(B, A)] =
     new Finite[(B, A)] {
       val cardinality = left.cardinality * self.cardinality 
-      def get(idx: Long) = 
+      def get(idx: BigInt) = 
         (left.get(idx / self.cardinality), self.get(idx % self.cardinality))
     }
+  
+  final def values: Seq[A] =
+    for (i <- BigInt(0) to (cardinality - 1)) yield get(i)
 }
 
 object Finite {
   final def empty[A]: Finite[A] =
     new Finite[A] {
-      val cardinality = 0l
-      def get(idx: Long) = throw new IndexOutOfBoundsException(idx.toString)
+      val cardinality = BigInt(0)
+      def get(idx: BigInt) = throw new IndexOutOfBoundsException(idx.toString)
     }
   
   final def singleton[A](x: A): Finite[A] =
     new Finite[A] {
-      val cardinality = 1l
-      def get(idx: Long) =
+      val cardinality = BigInt(1)
+      def get(idx: BigInt) =
         if (idx == 0) x else throw new IndexOutOfBoundsException(idx.toString)
     }
 }
 
 sealed abstract class Enumeration[+A] { self =>
   def parts: Stream[Finite[A]]
-  final def index(idx: Long): A = {
+  final def index(idx: BigInt): A = {
     var i = idx
     var p = parts
     while (!p.isEmpty) {
@@ -84,13 +87,13 @@ sealed abstract class Enumeration[+A] { self =>
   
   final def convolute[B](reverseYs: => Enumeration[B] with Enumeration.Reversed): Finite[(A, B)] = {
     lazy val combined = 
-      (self.parts zip reverseYs.parts).foldLeft((0l, Finite.empty[(A, B)])){
+      (self.parts zip reverseYs.parts).foldLeft((BigInt(0), Finite.empty[(A, B)])){
         case ((card, elems), (x, y)) =>
           (card + x.cardinality * y.cardinality, elems :+: (x :*: y))
       }  
     new Finite[(A, B)] {
       lazy val cardinality = combined._1
-      def get(idx: Long) =
+      def get(idx: BigInt) =
         combined._2.get(idx)
     }
   }
@@ -114,7 +117,10 @@ sealed abstract class Enumeration[+A] { self =>
     productRev(y.reversals)
   
   final def pay: Enumeration[A] =
-    new Enumeration[A] { lazy val parts = Finite.empty #:: self.parts } 
+    new Enumeration[A] { lazy val parts = Finite.empty #:: self.parts }
+  
+  final def values: Stream[(BigInt, Seq[A])] =
+    parts map (p => (p.cardinality, p.values))
 }
 
 object Enumeration {
@@ -126,5 +132,40 @@ object Enumeration {
   
   final def singleton[A](x: A) = new Enumeration[A] {
     lazy val parts = Finite.singleton(x) #:: Stream.empty        
+  }
+  
+  final lazy val boolEnumeration: Enumeration[Boolean] =
+    new Enumeration[Boolean] {
+      lazy val parts = new Finite[Boolean] {
+        val cardinality = BigInt(2)
+        def get(idx: BigInt) =
+          idx.toInt match {
+            case 0 => true
+            case 1 => false
+            case _ => throw new IndexOutOfBoundsException()
+          }
+        } #:: Stream.empty
+    }
+  
+  final lazy val intEnumeration: Enumeration[Int] =
+    new Enumeration[Int] {
+      lazy val parts = new Finite[Int] {
+          val cardinality = BigInt(Int.MaxValue) + (-1) * BigInt(Int.MinValue) + 1
+          def get(idx: BigInt) =
+            if (idx >= 0 && idx < cardinality) {
+              (idx % 2 + (if (idx % 2 > 0) 1l else -1l) * (idx / 2)).toInt
+            } else throw new IndexOutOfBoundsException()
+        } #:: Stream.empty
+  }
+  
+  final lazy val charEnumeration: Enumeration[Char] =
+    new Enumeration[Char] {
+      lazy val parts = new Finite[Char] {
+          val cardinality = BigInt(Char.MaxValue) + (-1) * BigInt(Char.MinValue) + 1
+          def get(idx: BigInt) =
+            if (idx >= 0 && idx < cardinality) {
+              idx.toChar
+            } else throw new IndexOutOfBoundsException()
+        } #:: Stream.empty
   }
 }
