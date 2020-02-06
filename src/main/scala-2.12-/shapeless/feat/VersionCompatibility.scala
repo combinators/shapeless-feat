27e46f5e2862eed1370380262f86df803ef5f355
JanBessai <jan.bessai@tu-dortmund.de>
2020-02-06 13:32:19 +0100

    Solve LazyList infinite recursion issue in Scala 2.13.x

--- src/main/scala-2.12-/shapeless/feat/VersionCompatibility.scala
+++ src/main/scala-2.12-/shapeless/feat/VersionCompatibility.scala
@@ -1,7 +1,7 @@
 package shapeless.feat
 
 object VersionCompatibility {
-  type LazyList[+A] = Stream[A]
+  type LazyList[+A] = scala.collection.immutable.Stream[A]
   private[feat] val LazyList = new StreamCompanionAdapter
 
   type Factory[-E, +C] = scala.collection.generic.CanBuildFrom[Nothing, E, C]
@@ -9,9 +9,10 @@ object VersionCompatibility {
 
 
   private[feat] class StreamCompanionAdapter {
-    def from[E](it: IterableOnce[E]): Stream[E] = it.toStream
+    def from[E](it: IterableOnce[E]): scala.collection.immutable.Stream[E] = it.toStream
   }
-  implicit def toStreamCompanion(adapter: StreamCompanionAdapter): Stream.type = Stream
+  implicit def toStreamCompanion(adapter: StreamCompanionAdapter): scala.collection.immutable.Stream.type = 
+    scala.collection.immutable.Stream
   implicit class NewBuilderOps[E, C](factory: Factory[E, C]) {
     def newBuilder: scala.collection.mutable.Builder[E, C] = factory.apply()
   }
